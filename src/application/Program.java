b8cd92122d24e5411a4c1ad7e60a24e3481bf2f2
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-01 10:56:04 -0300

    Comentarios na finalização

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,29 +15,30 @@ public class Program {
 
 		try (BufferedReader leitor = new BufferedReader(
 				new FileReader("D:\\MegaSync\\Docs e Projetos\\Java\\in.txt"))) {
-
+				//LEITOR
 			Map<String, Integer> candidatos = new HashMap<>();
-
+			// O MAP EM HASH
 			String line = leitor.readLine();
-
+			//LER A LINHA
 			while (line != null) {
 				String[] fields = line.split(",");
 				String nome = fields[0];
 				Integer votos = Integer.parseInt(fields[1]);
-
+				//MANJADINHO PRA DIVIDIR EM UM VETOR
 				if (candidatos.containsKey(nome)) {
+					//SE JA EXISTIR O NOME DENTRO DA LISTA, ELE PEGA E SOMA OS VOTOS
 					Integer votosomado = candidatos.get(nome) + votos;
 					candidatos.put(nome, votosomado);
 				}
-				else {
+				else {// SENAO, ELE INSERE
 					candidatos.put(nome, votos);
 				}
-
+				//LE A PROXIMA LINHA
 				line = leitor.readLine();
 
 			}
 
-			for (String chave : candidatos.keySet()) {
+			for (String chave : candidatos.keySet()) { //FOR PARA PERCORRER E EXIBIR
 				System.out.println(chave + ": " + candidatos.get(chave));
 			}
 
